98a6d2720792080aacd058057050bd8e991b10c0
Bruno <brunonasccimentto@gmail.com>
2024-02-07 23:21:38 -0300

    feat: add /table and /student/status routes

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { UpdateStudentStatusController } from './controllers/update-student-status-controller';
+import { GetTableController } from './controllers/get-table-controller';
+
+const routes = Router();
+const updateStudentStatusController = new UpdateStudentStatusController();
+const getTableController = new GetTableController()
+
+routes.post('/student/status', updateStudentStatusController.updateData);
+routes.get('/table', getTableController.getData);
+
+export { routes };
