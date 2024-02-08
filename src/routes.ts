51ff3e4b3d115321437725e6bf7edb8daf0c972a
Bruno <brunonasccimentto@gmail.com>
2024-02-07 23:26:56 -0300

    feat: add /table and /student/status routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import { GetTableController } from './controllers/get-table-controller';
 
 const routes = Router();
 const updateStudentStatusController = new UpdateStudentStatusController();
-const getTableController = new GetTableController()
+const getTableController = new GetTableController();
 
 routes.post('/student/status', updateStudentStatusController.updateData);
 routes.get('/table', getTableController.getData);
